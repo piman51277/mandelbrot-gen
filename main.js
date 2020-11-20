db4b6c73dd1729327e6766d2e966e7f9eb86a18e
piman <aidenleepi@gmail.com>
2020-11-20 13:16:52 -0500

    hello

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let cluster = require('cluster')
 const os = require('os')
 const cpuCount = os.cpus().length
-let size = 200;
+let size = 100000;
+const wsize = Math.floor(size * 1.25)
 if (cluster.isMaster) {
     const {
         createCanvas
     } = require('canvas')
-    const canvas = createCanvas(size, Math.floor(size * 1.5))
+    const canvas = createCanvas(wsize, size)
     let ctx = canvas.getContext('2d')
     let fs = require('fs');
 
-    const colors = new Array(16).fill(0).map((_, i) => `#${(i*16).toString(16).padEnd(6,0)}`)
+    const colors = new Array(16).fill(0).map((_, i) => `#00${((i*16).toString(16)).padEnd(4,0)}`)
 
 
     //divides up pixels for processing
@@ -20,31 +21,33 @@ if (cluster.isMaster) {
     }
 
     let nextLine = 0;
-    let finishCount=0;
+    let finishCount = 0;
     cluster.on('online', (worker) => {
         worker.send(JSON.stringify({
             'line': nextLine
         }))
-        console.log(`Processing (${nextLine}/${Math.floor(size*1.5)})`)
+        console.log(`Processing (${nextLine}/${size})`)
+
         nextLine++
 
         worker.on('message', function (msg) {
             msg = JSON.parse(msg)
             for (let i = 0; i < msg.data.length; i++) {
                 ctx.fillStyle = colors[msg.data[i][1] ? 0 : (msg.data[i][0] % colors.length - 1) + 1]
-                ctx.fillRect(i, msg.line, 1, 1)
+                ctx.fillRect(i,msg.line, 1, 1)
+
             }
-            if (nextLine <= Math.floor(size * 1.5)) {
+            if (nextLine <= size) {
                 worker.kill();
                 cluster.fork();
             } else {
-                const out = fs.createWriteStream(__dirname + '/mandelbrot.png')
+                const out = fs.createWriteStream(__dirname + `/mandelbrot${size}x${wsize}.png`)
                 const stream = canvas.createPNGStream()
                 stream.pipe(out)
                 out.on('finish', () => {
                     //this section will run the # of CPU cores, so wait until this is executed 8 times.
                     finishCount++;
-                    if(finishCount==cpuCount){
+                    if (finishCount == cpuCount) {
                         console.log('finished!')
                         process.exit();
                     }
@@ -55,7 +58,7 @@ if (cluster.isMaster) {
 
 } else {
     //mandelbrot worker
-    const MAX_ITERATION = size
+    const MAX_ITERATION = 200
 
     function mandelbrot(c) {
         let z = {
@@ -85,10 +88,10 @@ if (cluster.isMaster) {
             'line': msg.line,
             'data': []
         }
-        for (let i = 0; i < size; i++) {
+        for (let i = 0; i < wsize; i++) {
             complex = {
-                x: -2 + (x / size) * 2,
-                y: -1 + (i / size) * 2
+                x: -2 + (i / size) * 2,
+                y: -1 + (x / size) * 2
             }
             res.data.push(mandelbrot(complex))
         }
